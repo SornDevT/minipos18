536cdaa602f11e296a9ad37f33e6c9f73bc468d9
SornDevT <sorndev.teach@gmail.com>
2024-08-19 21:10:51 +0700

    Created Project Laravel + Vue VDO 01

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>App-Vue V3 111111111111</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MiniPos 18</title>
+
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+
+    <h1>Laravel App yirpeyioprueyitreuytoir</h1>
+    <hr>
+
+    <div id="app"></div>
+    
+
+    @vite('resources/js/app.js')
+</body>
+</html>
